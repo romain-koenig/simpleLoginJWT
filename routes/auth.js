441304ce777a84d46fdebc58f15fe93d26e1dea2
Romain KOENIG <romain.koenig@gmail.com>
2020-07-27 23:03:20 +0200

    Checking if user already exists before adding

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,23 @@
 const router = require('express').Router();
 const User = require('../model/user');
 
-const validation = require('../validation');
+const {registerValidation, loginValidation} = require('../validation');
 
 router.post('/register', async (req, res) => {
 
   //Data validation before saving.
-  const { error } = validation.registerValidation(req.body);
-  
+  const { error } = registerValidation(req.body);
+
   if (error) {
     return res.status(400).send(error.details[0].message)
+  }
+
+  //Check if user is already in database
+
+  const emailAlreadyExists = await User.findOne({email: req.body.email})
+
+  if (emailAlreadyExists) {
+    return res.status(400).send('email already exists');
   }
 
   console.log('Into API /register')
