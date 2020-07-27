bc8dc9a857c153f7bf32a6ab3667b8726b35ea83
Romain KOENIG <romain.koenig@gmail.com>
2020-07-27 22:37:12 +0200

    Data validation with Joi

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
 const User = require('../model/user');
 
+//validation
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+});
+
 router.post('/register', async (req, res) => {
+
+  //Data validation before asving.
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message)
+  }
   console.log('Into API /register')
   const user = new User(
     {
